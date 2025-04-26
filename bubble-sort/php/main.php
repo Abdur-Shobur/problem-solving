b498a73182d0803e2e1fe108a430d360637b4ad6
abdur-shobur <abdur.shobur.me@gmail.com>
2025-04-26 07:53:31 +0600

    PHP: bubble sort

--- bubble-sort/php/main.php
+++ bubble-sort/php/main.php
@@ -1,3 +1,40 @@
 <?php
+$my_array = [1, 4, 32, 25, 23, 252, 34, 26, 31, 2, 23, 45, 12, 34, 23, 4, 2, 12, 34, 3, 24, 22, 34, 3, 24];
 
-echo 'Print';
+function bubbleSort($array)
+{
+
+    $n = count($array);
+    for ($i = 0; $i < $n - 1; $i++) {
+        for ($j = 0; $j < $n - 1 - $i; $j++) {
+            if ($array[$j] > $array[$j + 1]) {
+                $temp          = $array[$j];
+                $array[$j]     = $array[$j + 1];
+                $array[$j + 1] = $temp;
+            }
+        }
+    }
+    return $array;
+}
+
+// $print = bubbleSort($my_array);
+// echo implode(", ", $print);
+
+function bubbleSortReverse($array)
+{
+    $n = count($array);
+
+    for ($i = 0; $i < $n - 1; $i++) {
+        for ($j = 0; $j < $n - 1 - $i; $j++) {
+            if ($array[$j] < $array[$j + 1]) {
+                $temp          = $array[$j];
+                $array[$j]     = $array[$j + 1];
+                $array[$j + 1] = $temp;
+            }
+        }
+
+    }
+    return $array;
+}
+$print = bubbleSortReverse($my_array);
+echo implode(", ", $print);
