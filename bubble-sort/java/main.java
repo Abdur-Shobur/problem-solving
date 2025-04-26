44accfd95729c3e3f26fd68c97c28c3905ab6170
abdur-shobur <abdur.shobur.me@gmail.com>
2025-04-26 08:28:47 +0600

    Java:Bubble sort

--- bubble-sort/java/main.java
+++ bubble-sort/java/main.java
@@ -1,5 +1,26 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        int[] my_array = { 1, 4, 32, 25, 23, 252, 34, 26, 31, 2, 23, 45, 12, 34, 23, 4, 2, 12, 34, 3, 24, 22, 34, 3,
+                24 };
+
+        int n = my_array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (my_array[j] > my_array[j + 1]) {
+                    int temp = my_array[j + 1];
+                    my_array[j + 1] = my_array[j];
+                    my_array[j] = temp;
+                }
+
+            }
+
+        }
+
+        for (int num : my_array) {
+            System.out.print(num + " ");
+        }
+
     }
+
 }
