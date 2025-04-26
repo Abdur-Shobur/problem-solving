37ec27c88aa8241d42de8642f096b2ec591f3601
abdur-shobur <abdur.shobur.me@gmail.com>
2025-04-26 08:08:45 +0600

    Python: Bubble sort

--- bubble-sort/python/main.py
+++ bubble-sort/python/main.py
@@ -1,1 +1,16 @@
-print('test print')
+my_array = [1, 4, 32, 25, 23, 252, 34, 26, 31, 2, 23,
+            45, 12, 34, 23, 4, 2, 12, 34, 3, 24, 22, 34, 3, 24]
+
+
+def bubbleSort(array):
+
+    n = len(array)
+    for i in range(n-1):
+        for j in range(n-1-i):
+            if (array[j] < array[j+1]):
+                array[j], array[j+1] = array[j+1], array[j]
+
+    return array
+
+
+print('sort ', bubbleSort(my_array))
