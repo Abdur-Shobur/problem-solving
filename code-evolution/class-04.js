a6e635e47785e3c377fff83710733f628bf9d9d3
abdur-shobur <abdur.shobur.me@gmail.com>
2025-05-06 08:39:58 +0600

    Try: DSA Codeeveluton class

---
+++ code-evolution/class-04.js
@@ -0,0 +1,12 @@
+/*
+-  Big O notation 
+    - time complexity 
+        - O(1)  constant 
+        - O(n)  linear 
+        - O(log n) logarithm 
+    - space complexity
+        - O(1) constrain
+        - O(n) linear
+        - O(log n) logarithm
+
+*/
--- selection-sort/js/index.js
+++ selection-sort/js/index.js
@@ -1,1 +1,21 @@
- 
+const selectionSort = () => {
+	let numbers = [5, 2, 9, 1, 5, 6];
+	let n = numbers.length;
+
+	for (let i = 0; i < n - 1; i++) {
+		let minIndex = i;
+
+		for (let j = i + 1; j < n; j++) {
+			if (numbers[j] < numbers[minIndex]) {
+				minIndex = j;
+			}
+		}
+
+		if (i !== minIndex) {
+			[numbers[i], numbers[minIndex]] = [numbers[minIndex], numbers[i]];
+		}
+	}
+	return numbers;
+};
+
+console.log(selectionSort());
