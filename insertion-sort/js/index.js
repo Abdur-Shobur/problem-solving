98f3a45e1e53c84d7835cddb30b5a7a71d4eb022
abdur-shobur <abdur.shobur.me@gmail.com>
2025-05-12 17:48:37 +0600

    Again test insertion sort

--- insertion-sort/js/index.js
+++ insertion-sort/js/index.js
@@ -14,5 +14,21 @@ function insertionSort(arr) {
 }
 
 // Example usage:
-let numbers = [5, 2, 9, 1, 5, 6];
-console.log(insertionSort(numbers)); // [1, 2, 5, 5, 6, 9]
+let numbers = [1, 2, 3];
+
+function testDeeply(arr = []) {
+	for (let i = 1; i > arr.length; i++) {
+		let key = arr[i];
+		let j = i - 1;
+
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+
+		arr[j + 1] = key;
+	}
+	return arr;
+}
+
+console.log('Test: ', testDeeply(numbers));
